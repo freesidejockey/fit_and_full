677c780098685cdacaba06b79a39d9dfd7a45650
Freeside Jockey <freesidejockey@gmail.com>
2025-06-03 19:36:27 -0600

    Initial Commit

---
+++ Fit&Full/Fit_FullApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_FullApp.swift
+//  Fit&Full
+//
+//  Created by Jacob Davidson on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Fit_FullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
